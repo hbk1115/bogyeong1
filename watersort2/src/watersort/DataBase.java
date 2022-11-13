99db319b8f850aa0357e13f8dae8e79749659f2b
jini_11 <jieun21124@naver.com>
2022-11-13 13:03:46 +0900

    로그인 기능 구현

---
+++ watersort2/src/watersort/DataBase.java
@@ -0,0 +1,52 @@
+package watersort;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase extends JFrame{
+	
+	private Connection conn;
+	private Statement stmt;
+	private final String url = "jdbc:mysql://localhost:3306/watersort?serverTimezone=UTC";
+	private final String id = "root";
+	private final String pw = "angel21124";
+	public DataBase() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, id, pw);
+			System.out.println("DB연결완료");
+			
+			stmt = conn.createStatement();
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC 드라이버 로드 오류");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+		}
+
+    }
+
+	public boolean checkLogIn(String userName, String password) {
+		try {
+			String logInSql = "select password from user where username='" + userName + "';";
+			
+			ResultSet result = stmt.executeQuery(logInSql);
+			
+			while (result.next()) {
+				if (password.equals(result.getString("password"))) {
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("로그인 SQL 오류");
+		}
+		
+		return false;
+	}
+}
---
+++ watersort2/src/watersort/LogIn.java
@@ -0,0 +1,117 @@
+package watersort;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LogIn extends JFrame{
+	
+	
+	JLabel title, nickName, password;
+	JTextField inputNickName, inputPassword;
+	JButton logIn, signUp, home;
+	
+	public LogIn() {
+		setTitle("LogIn");
+        setSize(500, 500);
+        setLayout(null);
+        
+        logInLayout();
+        
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+	}
+	
+	public void logInLayout() {
+		Font font = new Font("Gothic", Font.BOLD, 15);
+		
+		title = new JLabel("로그인");
+		nickName = new JLabel("닉네임");
+		password = new JLabel("비밀번호");
+		
+		inputNickName = new JTextField();
+		inputPassword = new JTextField();
+		
+		logIn = new JButton("로그인");
+		signUp = new JButton("회원가입");
+		home = new JButton("홈");
+		
+		title.setFont(new Font("Gothic", Font.BOLD, 40));
+		nickName.setFont(font);
+		password.setFont(font);
+		logIn.setFont(font);
+		signUp.setFont(font);
+		home.setFont(font);
+		
+		title.setBounds(180, 100, 300, 50);
+		nickName.setBounds(80, 200, 70, 30);
+		password.setBounds(80, 240, 70, 30);
+		inputNickName.setBounds(200, 200, 200, 30);
+		inputPassword.setBounds(200, 240, 200, 30);
+		logIn.setBounds(80, 300, 100, 30);
+		signUp.setBounds(190, 300, 100, 30);
+		home.setBounds(300, 300, 100, 30);
+		
+		add(title);
+		add(nickName);
+		add(password);
+		add(inputNickName);
+		add(inputPassword);
+		add(logIn);
+		add(signUp);
+		add(home);
+		
+		
+		logIn.addActionListener(new LogInListener());
+		signUp.addActionListener(new SignUpListener());
+		home.addActionListener(new HomeListener());
+		
+	}
+	
+	class LogInListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String userName = inputNickName.getText();
+			String userPassword = inputPassword.getText();
+			
+			if (userName.equals("") || userPassword.equals("")) {
+				JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 입력해주세요.", "로그인 실패", JOptionPane.ERROR_MESSAGE);
+			} else {
+				DataBase db = new DataBase();
+				if (db.checkLogIn(userName, userPassword)) {
+					JOptionPane.showMessageDialog(null, "로그인에 성공하셨습니다.");
+					setVisible(false);
+					new Main2();
+				} else {
+					JOptionPane.showMessageDialog(null, "일치하는 계정 정보가 없습니다.", "로그인 실패", JOptionPane.ERROR_MESSAGE);
+					//setVisible(false);
+					//new SignUp();
+				}
+			}
+			
+		}
+		
+	}
+	
+	class SignUpListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			setVisible(false);
+			//new SignUp();
+		}
+		
+	}
+	
+	class HomeListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			setVisible(false);
+			new Main();
+		}
+		
+	}
+}
