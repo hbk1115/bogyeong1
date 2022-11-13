856412e4c88d7bc9a59c6ecf3b017fb44188a81e
jini_11 <jieun21124@naver.com>
2022-11-13 13:03:18 +0900

    메인 페이지 레이아웃 수정

--- watersort2/src/watersort/Main.java
+++ watersort2/src/watersort/Main.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 
 public class Main extends JFrame{
 	
+	JLabel jLabel;
 	JPanel jPanel;
     JButton btn1;
     JButton btn2;
@@ -18,17 +19,28 @@ public class Main extends JFrame{
 	public void Ma() {
 		
         setTitle("Main");
-        setSize(500, 500);
+        setLayout(null);
+        
         jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(3, 1, 10, 10));
         btn1 = new JButton("로그인");
         btn2 = new JButton("회원가입");
-        btn3 = new JButton("뒤로");
+        btn3 = new JButton("게임 종료");
         
         jPanel.add(btn1);
         jPanel.add(btn2);
         jPanel.add(btn3);
-        add(jPanel);
+        jPanel.setBounds(120, 180, 250, 200);
         
+        jLabel = new JLabel("WaterSort Game!!");
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        jLabel.setFont(new Font("Gothic", Font.BOLD, 50));
+        jLabel.setBounds(0, 50, 500, 100);
+        
+        add(jLabel);
+        add(jPanel);
+         
+        setSize(500,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
@@ -38,8 +50,8 @@ public class Main extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
             	setVisible(false);
-                new Main2();
-                
+                //new Main2();
+                new LogIn();
             }
         });
         
