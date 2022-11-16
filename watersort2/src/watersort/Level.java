33d085fa197fb91e665a9eeee3a253f0efed3a52
jini_11 <jieun21124@naver.com>
2022-11-16 12:07:18 +0900

    레벨 레이아웃 수정

--- watersort2/src/watersort/Level.java
+++ watersort2/src/watersort/Level.java
@@ -3,36 +3,74 @@ package watersort;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Level extends JFrame{
+	
+	private JPanel levelPanel;
+	private JLabel levelTitle;
+	private JButton backBtn;
+	private JButton[] levelBtn;
+	
+	private List<String> level;
+	
+	private final int MAX_LEVEL = 6;
 	
 	public Level() {
 		 
         this.setTitle("Level");
-        JPanel jPanel = new JPanel();
-        JButton btn1 = new JButton("Level1");
-        JButton btn3 = new JButton("뒤로");
+        setLayout(null);
+        
+        levelPanel = new JPanel();
+        levelTitle = new JLabel("LEVEL");
+        level = new ArrayList<>();
+        levelBtn = new JButton[MAX_LEVEL];
+        backBtn = new JButton("뒤로");
+        
+        for (int i = 1; i <= levelBtn.length; i++) {
+        	level.add("Level" + i);	
+        }
+        
+        for (int i = 0; i < levelBtn.length; i++) {
+        	levelBtn[i] = new JButton(level.get(i));
+        }
+        
+        int row = level.size() / 3;
+        levelPanel.setLayout(new GridLayout(row,3,10,10));
+        levelPanel.setBounds(15, 100, 450, row * 50);
+        
+        levelTitle.setFont(new Font("Gothic", Font.BOLD, 40));
+        levelTitle.setBounds(180, 20, 300, 40);
+        
+        backBtn.setBounds(15, 400, 100, 30);
+        
+        for (int i = 0; i < levelBtn.length; i++) {
+        	levelPanel.add(levelBtn[i]);
+        }       
+        
+        add(levelTitle);
+        add(levelPanel);
+        add(backBtn);
         
         setSize(500, 500);
-        jPanel.add(btn1);
-        jPanel.add(btn3);
-        add(jPanel);
-        
         setLocationRelativeTo(null);
  
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         
-        btn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	setVisible(false);
-            	new Level1();
-                
-            }
-        });
+        for (int i = 0; i < levelBtn.length; i++) {
+	        levelBtn[i].addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	            	setVisible(false);
+	            	new Level1();
+	                
+	            }
+	        });
+        }
         
-        btn3.addActionListener(new ActionListener() {
+        backBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             	setVisible(false);
